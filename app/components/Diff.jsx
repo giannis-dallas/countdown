38bd16577683e49bcfa3ec1286b461a8a75975ab
Giannis Dallas <giannisdallas81@hotmail.com>
2024-02-05 23:05:24 +0200

    edits

--- app/components/Diff.jsx
+++ app/components/Diff.jsx
@@ -1,18 +1,24 @@
 import { differenceInDays, format } from 'date-fns';
 
-export default function Diff({props}) {
+export default function Diff({name, end}) {
 
-    const startDate = new Date();
-    const endDate = new Date(2024,11,15);
     
+    const startDate = new Date();
+    const endDate = new Date(end);
+    
+    console.log(end);
+    console.log(startDate);
+    console.log(endDate);
+
     const daysDifference = differenceInDays(endDate, startDate);
     console.log(`Difference in days: ${daysDifference}`);
     console.log(daysDifference);
 
     return (
     <>
-        <div className="diff-card bg-brilliant-rose-300 p-16 text-lg grid justify-center items-center gap-4">
-            Πόσες μέρες μέχρι τα Γενέθλιά μου;
+        <div className="diff-card bg-brilliant-rose-300 p-16 text-lg grid justify-center items-center gap-4 my-4">
+            <small>Πόσες μέρες μέχρι?</small>
+            <p>{name}</p>
             <p className='text-4xl mt-4'>
             {daysDifference}
             </p>
--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
       <div className="container">
-        <Diff date=""></Diff>
+        <Diff name="Γενέθλιά μου" end="12,15"></Diff>
+        <Diff name="Γιορτή μου" end="2024,12,9"></Diff>
       </div>
     </main>
   );
