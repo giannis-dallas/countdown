e85cc88f382b76532c573aef2a04bacdb87be1a3
Giannis Dallas <giannisdallas81@hotmail.com>
2024-02-21 23:31:59 +0200

    updat server

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
+
+const PORT = process.env.PORT || 3000; // Render.com will provide the port via environment variable
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
